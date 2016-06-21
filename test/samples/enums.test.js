de74b13d01159f229a661d61ec25f2bec4e9e145
dvoikinalex <94dvoikin@gmail.com>
2016-06-21 11:28:32 +0300

    converted tests in pip-webui modules to bdd

--- test/samples/enums.test.js
+++ test/samples/enums.test.js
@@ -1,29 +1,28 @@
-//'use strict';
-//
-//suite('pipEnums', function () {
-//    var pipEnums;
-//
-//    setup(module('pipEnums'));
-//
-//    setup(inject(function(_pipEnums_) {
-//       pipEnums = _pipEnums_;
-//    }));
-//
-//    test('SHARE_LEVEL', function (done) {
-//        assert.isDefined(pipEnums.SHARE_LEVEL);
-//
-//        done();
-//    });
-//
-//    test.only('GENDER', function (done) {
-//        assert.isDefined(pipEnums.GENDER);
-//
-//        assert.isDefined(pipEnums.GENDERS);
-//        assert.lengthOf(pipEnums.GENDERS, 3);
-//        assert.include(pipEnums.GENDERS, 'male');
-//        assert.include(pipEnums.GENDERS, 'female');
-//        assert.include(pipEnums.GENDERS, 'n/s');
-//
-//        done();
-//    });
-//});
+'use strict';
+
+describe('pipEnums', function () {
+    var pipEnums;
+
+    beforeEach(module('pipRest.Enums'));
+
+    beforeEach(inject(function(_pipEnums_) {
+        pipEnums = _pipEnums_;
+    }));
+
+
+
+    it('SHARE_LEVEL', function () {
+        assert.isDefined(pipEnums.SHARE_LEVEL);
+    });
+
+    it('GENDER', function () {
+        assert.isDefined(pipEnums.GENDER);
+
+        assert.isDefined(pipEnums.GENDERS);
+        assert.lengthOf(pipEnums.GENDERS, 3);
+        assert.include(pipEnums.GENDERS, 'male');
+        assert.include(pipEnums.GENDERS, 'female');
+        assert.include(pipEnums.GENDERS, 'n/s');
+
+    });
+});
