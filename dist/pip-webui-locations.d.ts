17ea2fdfe0f2e28f86c28b70c85f7212a1b6a46b
Anastas Fonotov <anastasf@gmail.com>
2017-10-27 19:50:31 +0300

    'rebuild'

--- dist/pip-webui-locations.d.ts
+++ dist/pip-webui-locations.d.ts
@@ -3,9 +3,6 @@ declare module pip.locations {
 
 
 
-let google: any;
-
-
 export interface ILocationDialogService {
     show(params: LocationDialogParams, successCallback?: any, cancelCallback?: any): void;
 }
@@ -16,6 +13,9 @@ export class LocationDialogParams {
     locationPos: any;
     locationName: string;
 }
+
+let google: any;
+
 
 }
 
