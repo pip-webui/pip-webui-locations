19873860c8f619d7d028382c7c9c0159b58622da
Aleksander Koshevierov <Iworb.ne@gmail.com>
2018-11-26 08:32:17 +0300

    'Rebuild'

--- dist/pip-webui-locations.d.ts
+++ dist/pip-webui-locations.d.ts
@@ -2,6 +2,8 @@ declare module pip.locations {
 
 
 
+let google: any;
+
 
 export interface ILocationDialogService {
     show(params: LocationDialogParams, successCallback?: any, cancelCallback?: any): void;
@@ -13,8 +15,6 @@ export class LocationDialogParams {
     locationPos: any;
     locationName: string;
 }
-
-let google: any;
 
 
 }
